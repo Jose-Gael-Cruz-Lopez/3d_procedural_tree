d9a0a428b59d0f44ffb489ea23194065d1717416
Jose Cruz <jose.colorstack@gmail.com>
2026-06-27 00:30:38 -0400

    Complete src/app/components/ui/alert-dialog.tsx (part 4/4)

--- src/app/components/ui/alert-dialog.tsx
+++ src/app/components/ui/alert-dialog.tsx
@@ -118,3 +118,40 @@ function AlertDialogDescription({
   );
 }
 
+function AlertDialogAction({
+  className,
+  ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Action>) {
+  return (
+    <AlertDialogPrimitive.Action
+      className={cn(buttonVariants(), className)}
+      {...props}
+    />
+  );
+}
+
+function AlertDialogCancel({
+  className,
+  ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Cancel>) {
+  return (
+    <AlertDialogPrimitive.Cancel
+      className={cn(buttonVariants({ variant: "outline" }), className)}
+      {...props}
+    />
+  );
+}
+
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+};
