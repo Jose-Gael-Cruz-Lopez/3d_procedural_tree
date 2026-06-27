3071fd39c9225614d3d8554a0a19c99c69ca9e12
Jose Cruz <jose.colorstack@gmail.com>
2026-06-27 00:30:40 -0400

    Add src/app/components/ui/popover.tsx (part 1/2)

---
+++ src/app/components/ui/popover.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import * as React from "react";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+
+import { cn } from "./utils";
+
+function Popover({
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Root>) {
+  return <PopoverPrimitive.Root data-slot="popover" {...props} />;
+}
+
+function PopoverTrigger({
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
+  return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />;
+}
+
+function PopoverContent({
+  className,
+  align = "center",
+  sideOffset = 4,
+  ...props
