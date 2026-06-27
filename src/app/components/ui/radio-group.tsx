7ec0d6295b58c56af8fbdef7a2ed984c5c683545
Jose Cruz <jose.colorstack@gmail.com>
2026-06-27 00:30:40 -0400

    Add src/app/components/ui/radio-group.tsx (part 1/2)

---
+++ src/app/components/ui/radio-group.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import * as React from "react";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+import { CircleIcon } from "lucide-react";
+
+import { cn } from "./utils";
+
+function RadioGroup({
+  className,
+  ...props
+}: React.ComponentProps<typeof RadioGroupPrimitive.Root>) {
+  return (
+    <RadioGroupPrimitive.Root
+      data-slot="radio-group"
+      className={cn("grid gap-3", className)}
+      {...props}
+    />
+  );
+}
+
+function RadioGroupItem({
+  className,
