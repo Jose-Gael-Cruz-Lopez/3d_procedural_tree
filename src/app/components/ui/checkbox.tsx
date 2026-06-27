546a0a5ab5d219f7061b5862308a1f63aedb25e5
Jose Cruz <jose.colorstack@gmail.com>
2026-06-27 00:30:38 -0400

    Add src/app/components/ui/checkbox.tsx (part 1/2)

---
+++ src/app/components/ui/checkbox.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { CheckIcon } from "lucide-react";
+
+import { cn } from "./utils";
+
+function Checkbox({
+  className,
+  ...props
+}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+  return (
+    <CheckboxPrimitive.Root
+      data-slot="checkbox"
+      className={cn(
