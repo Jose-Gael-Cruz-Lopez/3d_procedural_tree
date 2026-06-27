79f85f7e07aeffc4f43e924a0f26942e019ca157
Jose Cruz <jose.colorstack@gmail.com>
2026-06-27 00:30:39 -0400

    Add src/app/components/ui/hover-card.tsx (part 1/2)

---
+++ src/app/components/ui/hover-card.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import * as React from "react";
+import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
+
+import { cn } from "./utils";
+
+function HoverCard({
+  ...props
+}: React.ComponentProps<typeof HoverCardPrimitive.Root>) {
+  return <HoverCardPrimitive.Root data-slot="hover-card" {...props} />;
+}
+
+function HoverCardTrigger({
+  ...props
+}: React.ComponentProps<typeof HoverCardPrimitive.Trigger>) {
+  return (
+    <HoverCardPrimitive.Trigger data-slot="hover-card-trigger" {...props} />
+  );
+}
+
+function HoverCardContent({
