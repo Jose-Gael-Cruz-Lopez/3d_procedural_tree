7b50e1d79cc61e58868c38418dd36bfcd9a7c7f1
Jose Cruz <jose.colorstack@gmail.com>
2026-06-27 00:30:38 -0400

    Complete src/app/components/ui/card.tsx (part 3/3)

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -60,3 +60,33 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
     />
   );
 }
+
+function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-content"
+      className={cn("px-6 [&:last-child]:pb-6", className)}
+      {...props}
+    />
+  );
+}
+
+function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-footer"
+      className={cn("flex items-center px-6 pb-6 [.border-t]:pt-6", className)}
+      {...props}
+    />
+  );
+}
+
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+};
